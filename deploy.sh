b2ec10593d11a171a09c9316399a768cb11db5b0
Kevin Porras <kporras07@gmail.com>
2020-05-01 17:16:55 -0600

    Initial deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 ROOT="$(cd "$(dirname "$0")" && pwd)"
 cd "$ROOT"
 
-pip3 install -r requirements
+pip3 install -r requirements.txt
 
 if [ -n "${GITHUB_REPOSITORY:-}" ] && ! git config --get user.name; then
   git config --global user.name "TravisCI"
@@ -14,19 +14,17 @@ if [ -n "${GITHUB_REPOSITORY:-}" ] && ! git config --get user.name; then
   eval "$(ssh-agent)"
   ssh-add <(echo "$GH_PAGES_DEPLOY_KEY")
   trap "ssh-agent -k" EXIT
+
+  mkdocs build
+  git clone git@github.com:${GITHUB_REPOSITORY}.git repo
+  cd repo
+  git checkout gh-pages
+  shopt -s extglob
+  rm -rf ./!(.git|.|..)
+  cp -r ../site/*
+  cp -r ../images .
+  cp -r ../CNAME .  
+  git add -A .
+  git commit -m "Committed from ${TRAVIS_COMMIT}"
+  git push origin gh-pages -f
 fi
-
-mkdocs build $ROOT
-
-git clone git@github.com:${GITHUB_REPOSITORY}.git repo
-cd repo
-git checkout gh-pages
-shopt -s extglob
-rm -rf ./!(.git|.|..)
-cp -r ../site/*
-cp -r ../images .
-cp -r ../CNAME .
-
-git add -A .
-git commit -m "Committed from ${TRAVIS_COMMIT}"
-git push origin gh-pages -f
