95c04b5a9068a79d688b6040503a8ac8f7fa977d
Kevin Porras <kporras07@gmail.com>
2020-05-01 18:11:58 -0600

    Try fixing deploy.

--- deploy.sh
+++ deploy.sh
@@ -13,16 +13,11 @@ then
 else
   git config --global user.name "TravisCI"
   git config --global user.email "TravisCI@users.noreply.github.com"
-  git remote rm origin
-  git remote add origin "git@github.com:${GITHUB_REPOSITORY}.git"
 
   mkdocs build
   ls -alh
-  git clone git@github.com:${GITHUB_REPOSITORY}.git repo
+  git clone -n -b gh-pages git@github.com:${GITHUB_REPOSITORY}.git repo
   cd repo
-  git checkout gh-pages
-  shopt -s extglob
-  rm -rf !(.git|.|..)
   cp -r ../site/*
   cp -r ../images .
   cp -r ../CNAME .
